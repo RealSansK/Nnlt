e3634f408305fc1da656562bd11d1770ff83cf98
RealSansK <151824465+RealSansK@users.noreply.github.com>
2024-03-04 11:40:04 +0700

    Add files via upload

---
+++ bai2.cpp
@@ -0,0 +1,48 @@
+// bai2.cpp : This file contains the 'main' function. Program execution begins and ends there.
+//
+
+#include <iostream>
+#include <string>
+#include <stdio.h>
+int main()
+{
+	int b, a;
+	std::cout << "Keo =1; Bua=2; Bao=3\n";
+	std::cout << "Nguoi 1st chon keo bua bao:";
+	std::cin >> x;
+	std::cout << " Nguoi 2nd chon keo bua bao:";
+	std::cin >> y;
+	if (x== 1, y == 2) {
+		std::cout << " Nguoi 2nd thang";
+	}
+	else if (x == 1, y == 3) {
+			std::cout << "Nguoi 1st thang";
+		}
+		else if (x == 2, y == 1) {
+				std::cout << "Nguoi 1st thang";
+			}
+			else if (x == 2, y == 3) {
+				std::cout << "Nguoi 2st thang";
+				}
+				else if (x == 3, y == 2) {
+						std::cout << "Nguoi 1st thang";
+					}
+					else if (x == 3, y == 1) {
+							std::cout<<"Nguoi 2st thang"; 
+						}
+						else {
+							std::cout << "Hoa";
+						}
+	return 0;
+}
+
+// Run program: Ctrl + F5 or Debug > Start Without Debugging menu
+// Debug program: F5 or Debug > Start Debugging menu
+
+// Tips for Getting Started: 
+//   1. Use the Solution Explorer window to add/manage files
+//   2. Use the Team Explorer window to connect to source control
+//   3. Use the Output window to see build output and other messages
+//   4. Use the Error List window to view errors
+//   5. Go to Project > Add New Item to create new code files, or Project > Add Existing Item to add existing code files to the project
+//   6. In the future, to open this project again, go to File > Open > Project and select the .sln file
