79bc18d7433ca346c13a13e4bf5edcb9b119819d
RealSansK <151824465+RealSansK@users.noreply.github.com>
2024-03-10 23:36:47 +0700

    Update bai2.cpp

--- bai2.cpp
+++ bai2.cpp
@@ -1,48 +1,22 @@
-// bai2.cpp : This file contains the 'main' function. Program execution begins and ends there.
-//
-
 #include <iostream>
-#include <string>
-#include <stdio.h>
-int main()
-{
-	int b, a;
-	std::cout << "Keo =1; Bua=2; Bao=3\n";
-	std::cout << "Nguoi 1st chon keo bua bao:";
-	std::cin >> x;
-	std::cout << " Nguoi 2nd chon keo bua bao:";
-	std::cin >> y;
-	if (x== 1, y == 2) {
-		std::cout << " Nguoi 2nd thang";
-	}
-	else if (x == 1, y == 3) {
-			std::cout << "Nguoi 1st thang";
-		}
-		else if (x == 2, y == 1) {
-				std::cout << "Nguoi 1st thang";
-			}
-			else if (x == 2, y == 3) {
-				std::cout << "Nguoi 2st thang";
-				}
-				else if (x == 3, y == 2) {
-						std::cout << "Nguoi 1st thang";
-					}
-					else if (x == 3, y == 1) {
-							std::cout<<"Nguoi 2st thang"; 
-						}
-						else {
-							std::cout << "Hoa";
-						}
-	return 0;
+using namespace std;
+
+int main() {
+    int player1, player2;
+
+    cout << "Chon 1 cho keo, 2 cho bua, 3 cho bao:" << endl;
+    cout << "Nguoi choi 1: ";
+    cin >> player1;
+    cout << "Nguoi choi 2: ";
+    cin >> player2;
+
+    if (player1 == player2) {
+        cout << "Hoa nhau!" << endl;
+    } else if ((player1 == 1 && player2 == 3) || (player1 == 2 && player2 == 1) || (player1 == 3 && player2 == 2)) {
+        cout << "Nguoi choi 1 thang!" << endl;
+    } else {
+        cout << "Nguoi choi 2 thang!" << endl;
+    }
+
+    return 0;
 }
-
-// Run program: Ctrl + F5 or Debug > Start Without Debugging menu
-// Debug program: F5 or Debug > Start Debugging menu
-
-// Tips for Getting Started: 
-//   1. Use the Solution Explorer window to add/manage files
-//   2. Use the Team Explorer window to connect to source control
-//   3. Use the Output window to see build output and other messages
-//   4. Use the Error List window to view errors
-//   5. Go to Project > Add New Item to create new code files, or Project > Add Existing Item to add existing code files to the project
-//   6. In the future, to open this project again, go to File > Open > Project and select the .sln file
