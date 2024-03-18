30413885891328490908139217b091190454421f
RealSansK <151824465+RealSansK@users.noreply.github.com>
2024-03-18 10:54:54 +0700

    Add files via upload

---
+++ lab2_bai11.cpp
@@ -0,0 +1,29 @@
+﻿#include <iostream>
+using namespace std;
+
+int main() {
+    float distance;
+    cout << "Nhập vào số km đã đi: ";
+    cin >> distance;
+
+    float fare = 0;
+    if (distance <= 1) {
+        fare = distance * 15000;
+    }
+    else if (distance > 1 && distance <= 10) {
+        fare = 10000 + (distance - 1) * 8000;
+    }
+    else if (distance > 10 && distance <= 30) {
+        fare = 10000 + 80000 + (distance - 10) * 6000;
+    }
+    else if (distance > 30) {
+        fare = 10000 + 80000 + 120000 + (distance - 30) * 4000;
+    }
+    else {
+        cout << "Quãng đường không hợp lệ!";
+        return 1;
+    }
+
+    cout << "Cước taxi cho " << distance << " km là: " << fare << " VND" << endl;
+    return 0;
+}
