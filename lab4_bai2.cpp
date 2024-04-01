aa8d00737d033f9e445f3c88778ad52bbd6d6192
RealSansK <151824465+RealSansK@users.noreply.github.com>
2024-04-01 10:01:47 +0700

    Add files via upload

---
+++ lab4_bai2.cpp
@@ -0,0 +1,48 @@
+﻿#include <iostream>
+using namespace std;
+
+// Hàm tính tích của hai ma trận
+void nhanmatran(int matran1[][100], int matran2[][100], int result[][100], int n) {
+    for (int i = 0; i < n; ++i) {
+        for (int j = 0; j < n; ++j) {
+            result[i][j] = 0;
+            for (int k = 0; k < n; ++k) {
+                result[i][j] += matran1[i][k] * matran2[k][j];
+            }
+        }
+    }
+}
+
+int main() {
+    int n;
+    cout << "Nhap kich thuoc ma tran: ";
+    cin >> n;
+
+    int matran1[100][100], matran2[100][100], result[100][100];
+    cout << "Nhap cac phan tu cua ma tran 1:\n";
+    for (int i = 0; i < n; ++i) {
+        for (int j = 0; j < n; ++j) {
+            cin >> matran1[i][j];
+        }
+    }
+
+    cout << "Nhap cac phan tu cua ma tran 2:\n";
+    for (int i = 0; i < n; ++i) {
+        for (int j = 0; j < n; ++j) {
+            cin >> matran2[i][j];
+        }
+    }
+
+    nhanmatran(matran1, matran2, result, n);
+
+    // In ma trận kết quả
+    cout << "Ma tran ket qua:\n";
+    for (int i = 0; i < n; ++i) {
+        for (int j = 0; j < n; ++j) {
+            cout << result[i][j] << " ";
+        }
+        cout << endl;
+    }
+
+    return 0;
+}
